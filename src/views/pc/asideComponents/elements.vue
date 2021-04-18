8704f395ebc02b1786cfe9164435d08f882a2112
guoshuai <406601626@qq.com>
2021-04-19 00:11:02 +0800

    修改aside

--- src/views/pc/asideComponents/elements.vue
+++ src/views/pc/asideComponents/elements.vue
@@ -1,17 +1,36 @@
 <template>
-  <el-row> 1111111111111 </el-row>
+  <el-space
+      direction="vertical"
+      alignment="stretch"
+      :size="20"
+      class="elements-container">
+    <!--居中标题-->
+    <el-row type="flex" align="middle">
+      <el-col :span="6">底部版权信息</el-col>
+      <el-col :span="18">
+        <el-switch v-model="PC.aside.name"  active-value="Y" inactive-value="N" :width="50"></el-switch>
+      </el-col>
+    </el-row>
+  </el-space>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import {defineComponent} from "vue";
 
 export default defineComponent({
-  data() {
-    return {};
+  setup(){
+
   },
-  methods: {},
+  methods: {
+    print(){
+      console.log(this.PC.aside)
+    }
+  },
 });
 </script>
 
 <style lang="scss" scoped>
+.elements-container{
+  color: #ffffff;
+}
 </style>
