dbf14fd4d2902c8dc028385ca9b3090a007fb3b8
gs <406601626@qq.com>
2021-04-19 11:34:34 +0800

    修改main组件

--- src/views/pc/main.vue
+++ src/views/pc/main.vue
@@ -1,20 +1,55 @@
 <template>
-  <div>pc main</div>
+  <div class="pc-main-container">
+    <el-button @click="change">change</el-button>
+    <collapse
+      
+      class="aside-content-wrap"
+     
+    >
+      <div class="aside-content-component"></div>
+    </collapse>
+  </div>
 </template>
 
 <script>
-import {defineComponent} from 'vue'
-
+import { defineComponent, ref } from "vue";
+import Collapse from "@/components/collapse.vue";
 export default defineComponent({
-  data() {
+  components: {
+    Collapse,
+  },
+  setup() {
+    const show = ref(false);
     return {
-    }
+      show,
+    };
   },
   methods: {
-
-  }
+    change() {
+      this.show = !this.show;
+    },
+  },
 });
 </script>
 
 <style lang="scss" scoped>
+.pc-main-container {
+  position: relative;
+  .aside-content-wrap {
+    height: 100%;
+    position: absolute;
+    left: 100%;
+    top: 0;
+    margin-left: 1px;
+    z-index: 2;
+    background: rgb(62, 63, 65);
+
+    .aside-content-component {
+      width: 440px;
+      height: 100%;
+      padding: 20px;
+      box-sizing: border-box;
+    }
+  }
+}
 </style>
