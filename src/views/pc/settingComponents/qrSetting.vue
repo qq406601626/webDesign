5c7018e5b2d48429da2e7d8bbc92b0fc71f82709
guoshuai <406601626@qq.com>
2021-04-23 00:06:16 +0800

    修改二维码设置

---
+++ src/views/pc/settingComponents/qrSetting.vue
@@ -0,0 +1,75 @@
+<template>
+  <el-space
+      direction="vertical"
+      alignment="stretch"
+      :size="20"
+      class="qr-setting-container"
+  >
+    <el-row type="flex" justify="center" style="font-size: 20px">
+      二维码
+    </el-row>
+    <el-divider style="margin-bottom: 0;margin-top: 0"></el-divider>
+    <el-row type="flex" align="middle" :gutter="0">
+      <el-col :span="8">二维码设置：</el-col>
+      <el-col :span="16">
+        <el-input></el-input>
+      </el-col>
+    </el-row>
+    <el-row type="flex" align="middle" :gutter="0">
+      <el-col :span="8">二维码预览：</el-col>
+      <el-col :span="16">
+        <el-input></el-input>
+      </el-col>
+    </el-row>
+    <el-row type="flex" align="middle" :gutter="0">
+      <el-col :span="8">二维码文字颜色：</el-col>
+      <el-col :span="16">
+        <el-input></el-input>
+      </el-col>
+    </el-row>
+    <el-row type="flex" align="middle" :gutter="0">
+      <el-col :span="8">二维码文字大小：</el-col>
+      <el-col :span="16">
+        <el-input></el-input>
+      </el-col>
+    </el-row>
+    <el-row type="flex" align="middle" :gutter="0">
+      <el-col :span="8">二维码文字加粗：</el-col>
+      <el-col :span="16">
+        <el-input></el-input>
+      </el-col>
+    </el-row>
+    <el-row type="flex" align="middle" :gutter="0">
+      <el-col :span="8">二维码文字描述：</el-col>
+      <el-col :span="16">
+        <el-input></el-input>
+      </el-col>
+    </el-row>
+    <el-row type="flex" align="middle" :gutter="0">
+      <el-col :span="8">二维码显示方式：</el-col>
+      <el-col :span="16">
+        <el-input></el-input>
+      </el-col>
+    </el-row>
+  </el-space>
+</template>
+
+<script>
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  components: {},
+  setup() {
+    return {};
+  },
+  computed: {},
+});
+</script>
+
+<style lang="scss" scoped>
+.qr-setting-container {
+  color: #ffffff;
+
+
+}
+</style>
