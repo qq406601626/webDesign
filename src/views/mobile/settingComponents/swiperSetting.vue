c8a010f57e755da92c878214f84f7cb25bbfff5c
guoshuai <406601626@qq.com>
2021-05-05 23:56:57 +0800

    去掉filelist

--- src/views/mobile/settingComponents/swiperSetting.vue
+++ src/views/mobile/settingComponents/swiperSetting.vue
@@ -76,10 +76,7 @@ export default defineComponent({
   setup() {
     return {
       gutter: 20,
-      fileList: [{
-        name: 'food.jpeg',
-        url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
-      },],
+      fileList: [],
       mainState
     }
   },
